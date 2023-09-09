5283d921131730be7ce4327340b0fa066aebb9cc
lddtoan <lddtoan@outlook.com>
2023-09-09 14:36:33 +0700

    update: simulation api

--- services/api/simulationApi.ts
+++ services/api/simulationApi.ts
@@ -2,6 +2,6 @@ import { Simulation } from "@/hooks/use-simulation";
 import { instance } from "./_base";
 
 export const initSimulation = async (simulation: Simulation) => {
-  const response = await instance.get("/init", { params: simulation });
+  const response = await instance.get("init", { params: simulation });
   return response.data;
 };
