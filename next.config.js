d8ffecbcc489f420010e0b4c36958459077b2d1d
lddtoan <lddtoan@outlook.com>
2023-09-12 21:28:13 +0700

    update: backend ip

--- next.config.js
+++ next.config.js
@@ -16,15 +16,6 @@ const nextConfig = {
       },
     ],
   },
-
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://34.27.165.161/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
